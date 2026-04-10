fa4ba0f40de4281ffbbfac90724668767f97c178
Kyosuke Achiwa <achiwa912@gmail.com>
2026-04-10 15:03:26 -0400

    added retrieve.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 CHROMA_PATH = "chroma"
 SOURCE_DOCUMENTS = ["data/md/vbs.md", "data/md/dl.md", "data/md/eintr.md"]
 DEFAULT_EMBEDDING = "all-minilm:latest"
+DEFAULT_LLM = "llama3.2:3b"
---
+++ retrieve.py
@@ -0,0 +1,45 @@
+import chromadb
+from chromadb.utils.embedding_functions.ollama_embedding_function import (
+    OllamaEmbeddingFunction,
+)
+import ollama
+from config import CHROMA_PATH, DEFAULT_EMBEDDING, DEFAULT_LLM
+
+def retrieve(query: str, col_name: str, top_k: int=3) -> dict:
+    try:
+        client = chromadb.PersistentClient(path=CHROMA_PATH)
+        col = client.get_collection(name=col_name)
+    except Exception as e:
+        raise(f'Does ChromaDB/{col_name} exist?: {e}')
+
+    results = col.query(
+        query_texts=query,
+        n_results=top_k
+    )
+    return {
+        "query": query,
+        "ids": results['ids'][0],
+        "chunks": results['documents'][0],
+        "distances": results['distances'][0]
+    }
+
+def answer(query: str, col_name: str, top_k: int=3, model=DEFAULT_LLM) -> dict:
+    retrieved = retrieve(query, col_name, top_k)
+    chunks_str = ''
+    for chunk in retrieved['chunks']:
+        chunks_str = chunks_str + '---\n' + chunk
+    prompt=f"Concisely answer the question in Query based only on the information in Context.  If you don't know the answer, just say 'I don't know'.\nQuery: {query}\nContext: {chunks_str.lstrip()}\nAnswer: "
+    
+    ans = ollama.generate(
+        model=model,
+        prompt=prompt
+    )
+    return {
+        "query": query,
+        "answer": ans['response'],
+        "ids": retrieved['ids'],
+        "chunks": retrieved['chunks'],
+        "distances": retrieved['distances']
+    }
+
+

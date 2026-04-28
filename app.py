a3b8be634bd348489224e7c2f8098617acb90b3d
Kyosuke Achiwa <achiwa912@gmail.com>
2026-04-28 07:40:35 -0400

    tailwind css and daisyui

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import glob
 import json
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify, request
+from config import EMBED_MODELS, ANS_MODELS
 
 app = Flask(__name__)
 
@@ -7,10 +9,28 @@ app = Flask(__name__)
 def index():
     return send_from_directory('.', 'index.html')
 
+
+@app.route("/api/files")
+def get_files():
+    return jsonify(glob.glob('results/*.jsonl'))
+
+
 @app.route("/api/results")
 def get_results():
-    with open('results/eval_20260424_081257.jsonl', 'r') as f:
-        items = []
+    file_path = request.args.get('file')
+    if not file_path:
+        return jsonify([])
+    items = []
+    with open(file_path, 'r') as f:
         for line in f:
             items.append(json.loads(line))
-    return items
+    return jsonify(items)
+
+
+@app.route("/api/models")
+def get_models():
+    models = {
+        "embed_models": EMBED_MODELS,
+        "ans_models": ANS_MODELS
+    }
+    return jsonify(models)
--- config.py
+++ config.py
@@ -2,10 +2,11 @@ from google import genai
 CHROMA_PATH = "chroma"
 RESULTS_PATH = "results"
 SOURCE_DOCUMENTS = ["data/org/vbs.org", "data/org/homelab.org", "data/org/advanced.org"]
-DEFAULT_EMBEDDING = "nomic-embed-text:latest"  # "all-minilm:latest"
-DEFAULT_LLM = "llama3.1:8b"
-DEFAULT_ANS_LLM = "gemini-2.5-flash-lite"  # "llama3.1:8b"
-DEFAULT_EVAL_LLM = "gemini-2.5-flash-lite"  # "llama3.1:8b"
+EMBED_MODELS = ["qwen3-embedding:0.6b", "all-minilm:latest", "nomic-embed-text:latest"]
+ANS_MODELS = ["gemini-2.5-flash-lite", "llama3.1:8b"]
+DEFAULT_EMBEDDING = EMBED_MODELS[0]
+DEFAULT_ANS_LLM = ANS_MODELS[0]
+DEFAULT_EVAL_LLM = ANS_MODELS[0]
 DEFAULT_RERANK = "cross-encoder/ms-marco-MiniLM-L6-v2"
 TOP_K_RERANK = 20
 genai_client = genai.Client()
